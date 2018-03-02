885da7843b7f692d0bc05681a448c55477da7507
Max <maxnelson997@gmail.com>
2018-03-02 15:09:09 -0700

    added git ignore to ignore node modules and changed the h1 tag in our app.js

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <h1>Bottega Mad Libs</h1>
       </div>
     );
   }
