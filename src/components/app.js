243eaa68dc258c06f146e799b3f02ab2440874ff
Max <maxnelson997@gmail.com>
2018-03-07 15:16:53 -0700

    created madlib form component

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import MadlibForm from './madlib_form';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <div className="backgroundSkew">
+
           <div className="madlib-heading">
             <h1>Bottega Mad Libs</h1>  
             <div className="madlib-subheading">
               Fill out the fields below and click the generate button<br/>to see the Mad Lib story.
             </div>
           </div>
+          <MadlibForm />
+
         </div>
       </div>
     );
---
+++ src/components/madlib_form.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class MadlibForm extends Component {
+    render() {
+      return <h1>Bottega Mad Libs Form Component</h1>;
+    }
+}
+
+export default MadlibForm;
